92717e1d1d2eb5cefdca95ea25e3fa1c3a963f34
Abdi Putrana Radian <57528253+abdipr@users.noreply.github.com>
2024-11-02 09:16:29 +0700

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Content-Type: application/json");
+echo json_encode(["status" => "200","author" => "abdiputranar","message" => "Check https://github.com/abdipr/myinstants-api for documentation"], JSON_PRETTY_PRINT);
+?>
