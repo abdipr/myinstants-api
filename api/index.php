879c0c65524076a550bc8506b5d729c83fb55e49
Abdi Putrana Radian <57528253+abdipr@users.noreply.github.com>
2024-11-02 12:15:19 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 header("Content-Type: application/json");
-echo json_encode(["status" => "200","author" => "abdiputranar","message" => "Check https://github.com/abdipr/myinstants-api for documentation"], JSON_PRETTY_PRINT);
+echo str_replace("\\", "", json_encode(["status" => "200","author" => "abdiputranar","message" => "Check https://github.com/abdipr/myinstants-api for documentation"], JSON_PRETTY_PRINT));
 ?>
